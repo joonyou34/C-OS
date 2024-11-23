66f2890c6b46ce00baed932572672ead5f133bc4
Parlerler <malekwa3er99@gmail.com>
2024-11-24 00:04:44 +0200

    Shared memory 4 done WIP
    Co-authored-by: MustafaBahnasy <Mostafa-Bahnasy@users.noreply.github.com>

--- kern/mem/shared_memory_manager.c
+++ kern/mem/shared_memory_manager.c
@@ -66,9 +66,12 @@ inline struct FrameInfo** create_frames_storage(int numOfFrames)
 {
 	//TODO: [PROJECT'24.MS2 - #16] [4] SHARED MEMORY - create_frames_storage()
 	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("create_frames_storage is not implemented yet");
+	//panic("create_frames_storage is not implemented yet");
 	//Your Code is Here...
 
+	struct FrameInfo** FI[number_of_frames];
+	memset(FI,0,sizeof(FI));
+	return FI;
 }
 
 //=====================================
@@ -81,9 +84,17 @@ struct Share* create_share(int32 ownerID, char* shareName, uint32 size, uint8 is
 {
 	//TODO: [PROJECT'24.MS2 - #16] [4] SHARED MEMORY - create_share()
 	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("create_share is not implemented yet");
+	//panic("create_share is not implemented yet");
 	//Your Code is Here...
 
+	//ID(va), ownerID (have), name(have), size(have), writable(have), framestorage (call create_frame_storage)
+
+	struct Share* Sobject;
+	*Sobject->name = shareName;
+	Sobject->references = 1;
+	Sobject->size = size;
+	Sobject->isWritable = isWritable;
+	return Sobject;
 }
 
 //=============================
@@ -97,9 +108,19 @@ struct Share* get_share(int32 ownerID, char* name)
 {
 	//TODO: [PROJECT'24.MS2 - #17] [4] SHARED MEMORY - get_share()
 	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("get_share is not implemented yet");
+	//panic("get_share is not implemented yet");
 	//Your Code is Here...
+	
+	struct Share* SobjectSearch;
+	LIST_FOREACH(SobjectSearch, &AllShares.shares_list)
+	{
+		if(SobjectSearch->ownerID == ownerID && strcmp(SobjectSearch->name, name) == 0)
+		{
+			return SobjectSearch;
+		}
+	}
 
+	return NULL;
 }
 
 //=========================
@@ -109,10 +130,42 @@ int createSharedObject(int32 ownerID, char* shareName, uint32 size, uint8 isWrit
 {
 	//TODO: [PROJECT'24.MS2 - #19] [4] SHARED MEMORY [KERNEL SIDE] - createSharedObject()
 	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("createSharedObject is not implemented yet");
+	//panic("createSharedObject is not implemented yet");
 	//Your Code is Here...
 
 	struct Env* myenv = get_cpu_proc(); //The calling environment
+
+	if(get_share(ownerID, shareName) != NULL)
+	{
+		return E_SHARED_MEM_EXISTS;
+	}
+
+	struct Share* Sobject = create_share(ownerID, shareName, size, isWritable);
+	Sobject->ID = (int32)((uint32)virtual_address & 0x7FFFFFFF); // mask MSB
+	LIST_INSERT_TAIL(&AllShares.shares_list, Sobject);
+
+	uint32 numOfPages = ROUNDUP(size, PAGE_SIZE) / PAGE_SIZE;
+
+	struct FrameInfo** SobjectFrameArr = create_frames_storage(numOfPages);
+	uint32 ind = 0;
+	for (uint32 addr = virtual_address; addr < (virtual_address + numOfPages * PAGE_SIZE); addr += PAGE_SIZE)
+	{
+		struct FrameInfo *frame_info = SobjectFrameArr[ind];
+		if (allocate_frame(&frame_info) || map_frame(ptr_page_directory, frame_info, addr, PERM_WRITEABLE))
+		{
+			// Roll back if allocation fails
+			for (uint32 rollback_addr = virtual_address; rollback_addr < addr; rollback_addr += PAGE_SIZE)
+			{
+				unmap_frame(ptr_page_directory, rollback_addr);
+			}
+			// Remove the added share
+			LIST_REMOVE(&AllShares.shares_list, AllShares.shares_list.lh_last);
+			return E_NO_SHARE;
+		}
+		ind++;
+	}
+	Sobject->framesStorage = SobjectFrameArr;
+	return Sobject->ID;
 }
 
 
